67fe161113933ad843315d1c59be6817cb1015d6
FarolDaPraia <aletsuno@googlemail.com>
2020-10-12 23:05:28 +0200

    Basic Python - YAML files

---
+++ py_yaml.py
@@ -0,0 +1,34 @@
+"""
+this script is used to exercise the yaml library.
+
+Author: FarolDaPraia
+Date: 10/12/2020
+"""
+
+import yaml
+
+
+def yaml_loader(filepath):
+    """Load a yaml file."""
+    with open(filepath, 'r') as file_descriptor:
+        data = yaml.load(file_descriptor)
+    return data
+
+
+def yaml_dump(filepath, data):
+    """Dump data to a yaml file."""
+    with open(filepath, 'w') as file_descriptor:
+        yaml.dump(data, file_descriptor)
+
+
+if __name__ == "__main__":
+    filepath = 'data.yaml'
+    data = yaml_loader(filepath)
+
+    metadata = data.get('person')
+    for metadata_name, metadata_value in metadata.items():
+        print(metadata_name, metadata_value)
+
+    filepath2 = 'test.yaml'
+    data2 = {'items': {'sword': 100, 'axe': 80, 'boots': 40}}
+    yaml_dump(filepath2, data2)
